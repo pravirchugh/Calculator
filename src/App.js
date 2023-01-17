ae66318b3c4c52e515fe6da5d399db562269a63c
pravirchugh <pravirchugh@yahoo.com>
2023-01-16 23:01:03 -0800

    Starting from scratch

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Calculator</h1>
+        <p>Made with React.js</p>
       </header>
-    </div>
+      <body>
+        
+      </body>
+    
+    </>
   );
 }
+
+// Different types of components:
+  // Number buttons (0-9)
+  // Operation buttons (+, -, *, /)
+  // Misc. buttons (+/-, Clear, Enter)
 
 export default App;
