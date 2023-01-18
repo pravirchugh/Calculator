2fdb828650c846bbe0d7f1a5f549e99a319c6d8f
pravirchugh <pravirchugh@yahoo.com>
2023-01-17 21:19:07 -0800

    Added most of the buttons, unstyled

--- src/App.js
+++ src/App.js
@@ -18,21 +18,49 @@ function App() {
         </div>
 
         <div className="buttons">
+
+          <div className="buttonRow">
+            <NumberButton val="+/-"></NumberButton>
+            <NumberButton val="%"></NumberButton>
+            <NumberButton val="C"></NumberButton>
+            <NumberButton val="⌫"></NumberButton>
+          </div>
+
+          <div className="buttonRow">
+            <NumberButton val="1/x"></NumberButton>
+            <NumberButton val="x^2"></NumberButton>
+            <NumberButton val="√"></NumberButton>
+            <NumberButton val="/"></NumberButton>
+          </div>
+
           <div className="buttonRow">
             <NumberButton val="7"></NumberButton>
             <NumberButton val="8"></NumberButton>
             <NumberButton val="9"></NumberButton>
+            <NumberButton val="*"></NumberButton>
           </div>
+
           <div className="buttonRow">
             <NumberButton val="4"></NumberButton>
             <NumberButton val="5"></NumberButton>
             <NumberButton val="6"></NumberButton>
+            <NumberButton val="-"></NumberButton>
           </div>
+
           <div className="buttonRow">
             <NumberButton val="1"></NumberButton>
             <NumberButton val="2"></NumberButton>
             <NumberButton val="3"></NumberButton>
+            <NumberButton val="+"></NumberButton>
           </div>
+
+          <div className="buttonRow">
+            <NumberButton val="0"></NumberButton>
+            <NumberButton val="."></NumberButton>
+            <NumberButton val="="></NumberButton>
+            
+          </div>
+
         </div>
 
 
