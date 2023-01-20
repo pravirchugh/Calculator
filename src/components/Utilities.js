b9870c7aeac9384fb82ac377e805e7cf31f25a85
pravirchugh <pravirchugh@yahoo.com>
2023-01-20 09:34:14 -0800

    Accounting for an occasional rounding error when taking square root, followed by square of a number

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -5,7 +5,17 @@ export function reciprocalOutput(output) {
     return (1 / parseFloat(output)).toString();
 }
 export function squareOutput(output) {
-    return (parseFloat(output) * parseFloat(output)).toString();
+    let temporaryValue = parseFloat(output) * parseFloat(output);
+
+    if(temporaryValue <= Math.floor(temporaryValue) + 0.0000001){ // this accounts for a precision error 
+        temporaryValue = Math.floor(temporaryValue);
+    }
+    if(temporaryValue >= Math.ceil(temporaryValue) - 0.0000001){ // this accounts for a precision error 
+        temporaryValue = Math.ceil(temporaryValue);
+    }
+    
+
+    return (temporaryValue).toString();
 }
 export function rootOutput(output) {
     return (Math.sqrt(parseFloat(output)).toString());
